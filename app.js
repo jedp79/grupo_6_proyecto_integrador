34d6549ff4e3abdec3a807a83477266d1bdd0003
Uriel <urielszw@gmail.com>
2021-09-13 19:56:17 -0300

    Creamos el home en Html y Css

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(3000, ()=>{
+    console.log('Servidor corriendo');
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+});
