c656ef33b0af95a1bbde804237a78892b76899cb
Gino <ginobrunori17@gmail.com>
2021-09-17 12:37:48 -0300

    Agrego El Carrito

--- app.js
+++ app.js
@@ -12,3 +12,15 @@ app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
+app.get('/carrito', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/carrito.html'));
+});
+
+app.get('/login', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.get('/register', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
